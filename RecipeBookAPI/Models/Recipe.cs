130a92c343d7137a625d3f1a705d0026134eef31
ismaC95 <ismaelcarayol@gmail.com>
2025-11-13 12:20:28 +0000

    Initial project commit - Recipe Book app structure

---
+++ RecipeBookAPI/Models/Recipe.cs
@@ -0,0 +1,18 @@
+﻿namespace RecipeBookAPI.Models
+{
+    public class Recipe
+    {
+        //Represents Recipe entity stored in the database
+        public int recipeID { get; set; }
+        public string name { get; set; }
+        public string instructions { get; set; }
+        public int prepTime { get; set; }
+        public int cookTime { get; set; }
+        public string difficulty { get; set; }
+        public DateTime dateCreated { get; set; }
+        public bool isPublic { get; set; }
+        //imageURL datatype has ? because it can be NULL (Nullable Reference Types)
+        public string? imageURL { get; set; }
+        public int ownerID { get; set; }
+    }
+}
---
+++ RecipeBookAPI/Services/RecipeServices.cs
@@ -0,0 +1,15 @@
+﻿using RecipeBookAPI.Models;
+
+namespace RecipeBookAPI.Services
+{
+    //Recipe class with all the business logic methods
+    //When in production, this class receives the requests from Users through controllers and will use the data from the database to provide the controllers with the requested information
+    public class RecipeServices
+    {
+        //Recipe is private so it can't be accesses from outside this class (encapsulation)
+        //using a List now but in the future will be a database table with Entitiy Framework (EF Core)
+        private static List<Recipe> _recipes = new List<Recipe>();
+
+
+    }
+}
